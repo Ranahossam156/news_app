a00d079685da01477cd12b99295f7885b820793c
Ranahossam156 <ranahossam156@gmail.com>
2023-09-05 02:39:14 +0200

    build categories list view

---
+++ lib/views/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/widgets/categories_list_view.dart';
+import 'package:news_app/widgets/category_card.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+      ),
+      body: const CategoriesListView(),
+    );
+  }
+}
+
