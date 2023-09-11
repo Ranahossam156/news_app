9af60b7bd284d9d26039f7448083a9511f34627f
Ranahossam156 <ranahossam156@gmail.com>
2023-09-11 05:30:38 +0200

    handle exceptions

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -6,19 +6,23 @@ class NewsServices {
 
   NewsServices(this.dio);
   Future<List<ArticleModel>> getNews() async {
-    Response response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=15984d87541e49579ab7e5de0e222972&category=general');
-    Map<String, dynamic> jsonData = response.data;
-    List<dynamic> articles = jsonData['articles'];
-    List<ArticleModel> articlesList = [];
-    for (var article in articles) {
-      ArticleModel articleModel = ArticleModel(
-        image: article['urlToImage'],
-        title: article['title'],
-        subTitle: article['description'],
-      );
-      articlesList.add(articleModel);
+    try {
+      Response response = await dio.get(
+          'https://newsapi.org/v2/top-headlines?country=us&apiKey=15984d87541e49579ab7e5de0e222972&category=general');
+      Map<String, dynamic> jsonData = response.data;
+      List<dynamic> articles = jsonData['articles'];
+      List<ArticleModel> articlesList = [];
+      for (var article in articles) {
+        ArticleModel articleModel = ArticleModel(
+          image: article['urlToImage'],
+          title: article['title'],
+          subTitle: article['description'],
+        );
+        articlesList.add(articleModel);
+      }
+      return articlesList;
+    } catch (e) {
+      return [];
     }
-    return articlesList;
   }
 }
