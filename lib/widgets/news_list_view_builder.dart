4b3cbd9666ae1c48c672c25a595a6b8a3182e0a9
Ranahossam156 <ranahossam156@gmail.com>
2023-09-11 06:00:29 +0200

    seprate the trigger part

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,26 +1,45 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_services.dart';
 import 'package:news_app/widgets/news_list_view.dart';
 
-class NewsListViewBuilder extends StatelessWidget {
+class NewsListViewBuilder extends StatefulWidget {
   const NewsListViewBuilder({
     super.key,
   });
 
   @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  var future;
+  @override
+  void initState() {
+    super.initState();
+    future =NewsServices(Dio()).getNews();
+  }
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: NewsServices(Dio()).getNews(),
+    return FutureBuilder<List<ArticleModel>>(
+      future: future ,
       builder: (context, snapShot) {
-        return NewsListView(
-          articles: snapShot.data ?? [],
-        );
+        if (snapShot.hasData) {
+          return NewsListView(
+            articles: snapShot.data!,
+          );
+        } else if (snapShot.hasError) {
+          return const SliverToBoxAdapter(
+              child: Text('oops there was an error , try later'));
+        } else {
+          return const SliverToBoxAdapter(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
       },
     );
-    // isLoading
-    //     ? const SliverToBoxAdapter(
-    //         child: Center(child: CircularProgressIndicator()))
-    //     : articles.isNotEmpty? NewsListView(articles: articles,):const SliverToBoxAdapter(child:Text('oops there was an error , try later'));
   }
 }
